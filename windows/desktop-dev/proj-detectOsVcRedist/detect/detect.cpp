c282dcb2e28fc16243bad8b9a49aae02dcffeb4c
lgy047 <lgy047@gmail.com>
2012-12-06 07:56:39 +0000

    添加检测操作系统版本 和 检测操作系统中安装的VC redist组件的情况

---
+++ windows/desktop-dev/proj-detectOsVcRedist/detect/detect.cpp
@@ -0,0 +1,389 @@
+// detectvcdist.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+#include <msi.h>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Msi.lib")
+#include <tchar.h>
+#include <stdio.h>
+#include <strsafe.h>
+
+
+
+#define PRODUCT_UNDEFINED                       0x00000000
+
+#define PRODUCT_ULTIMATE                        0x00000001
+#define PRODUCT_HOME_BASIC                      0x00000002
+#define PRODUCT_HOME_PREMIUM                    0x00000003
+#define PRODUCT_ENTERPRISE                      0x00000004
+#define PRODUCT_HOME_BASIC_N                    0x00000005
+#define PRODUCT_BUSINESS                        0x00000006
+#define PRODUCT_STANDARD_SERVER                 0x00000007
+#define PRODUCT_DATACENTER_SERVER               0x00000008
+#define PRODUCT_SMALLBUSINESS_SERVER            0x00000009
+#define PRODUCT_ENTERPRISE_SERVER               0x0000000A
+#define PRODUCT_STARTER                         0x0000000B
+#define PRODUCT_DATACENTER_SERVER_CORE          0x0000000C
+#define PRODUCT_STANDARD_SERVER_CORE            0x0000000D
+#define PRODUCT_ENTERPRISE_SERVER_CORE          0x0000000E
+#define PRODUCT_ENTERPRISE_SERVER_IA64          0x0000000F
+#define PRODUCT_BUSINESS_N                      0x00000010
+#define PRODUCT_WEB_SERVER                      0x00000011
+#define PRODUCT_CLUSTER_SERVER                  0x00000012
+#define PRODUCT_HOME_SERVER                     0x00000013
+#define PRODUCT_STORAGE_EXPRESS_SERVER          0x00000014
+#define PRODUCT_STORAGE_STANDARD_SERVER         0x00000015
+#define PRODUCT_STORAGE_WORKGROUP_SERVER        0x00000016
+#define PRODUCT_STORAGE_ENTERPRISE_SERVER       0x00000017
+#define PRODUCT_SERVER_FOR_SMALLBUSINESS        0x00000018
+#define PRODUCT_SMALLBUSINESS_SERVER_PREMIUM    0x00000019
+
+#define PRODUCT_UNLICENSED                      0xABCDABCD
+
+
+
+#define VER_SERVER_NT                       0x80000000
+#define VER_WORKSTATION_NT                  0x40000000
+#define VER_SUITE_SMALLBUSINESS             0x00000001
+#define VER_SUITE_ENTERPRISE                0x00000002
+#define VER_SUITE_BACKOFFICE                0x00000004
+#define VER_SUITE_COMMUNICATIONS            0x00000008
+#define VER_SUITE_TERMINAL                  0x00000010
+#define VER_SUITE_SMALLBUSINESS_RESTRICTED  0x00000020
+#define VER_SUITE_EMBEDDEDNT                0x00000040
+#define VER_SUITE_DATACENTER                0x00000080
+#define VER_SUITE_SINGLEUSERTS              0x00000100
+#define VER_SUITE_PERSONAL                  0x00000200
+#define VER_SUITE_BLADE                     0x00000400
+#define VER_SUITE_EMBEDDED_RESTRICTED       0x00000800
+#define VER_SUITE_SECURITY_APPLIANCE        0x00001000
+#define VER_SUITE_STORAGE_SERVER            0x00002000
+#define VER_SUITE_COMPUTE_SERVER            0x00004000
+
+#define SM_TABLETPC             86
+#define SM_MEDIACENTER          87
+#define SM_STARTER              88
+#define SM_SERVERR2             89
+
+
+BOOL IsWow64()
+{
+	BOOL bIsWow64 = FALSE;
+	typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
+	LPFN_ISWOW64PROCESS fnIsWow64Process;
+	fnIsWow64Process = (LPFN_ISWOW64PROCESS)GetProcAddress(GetModuleHandle(TEXT("kernel32")), "IsWow64Process");
+	if (NULL != fnIsWow64Process)
+	{
+		if (!fnIsWow64Process(GetCurrentProcess(), &bIsWow64))
+		{
+			//出现错误
+		}
+	}
+	return bIsWow64;
+}
+#pragma comment(lib, "User32.lib")
+
+#define BUFSIZE 256
+
+typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
+typedef BOOL (WINAPI *PGPI)(DWORD, DWORD, DWORD, DWORD, PDWORD);
+
+BOOL GetOSDisplayString( LPTSTR pszOS)
+{
+	OSVERSIONINFOEX osvi;
+	SYSTEM_INFO si;
+	PGNSI pGNSI;
+	PGPI pGPI;
+	BOOL bOsVersionInfoEx;
+	DWORD dwType;
+
+	ZeroMemory(&si, sizeof(SYSTEM_INFO));
+	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	bOsVersionInfoEx = GetVersionEx((OSVERSIONINFO*) &osvi);
+
+	if(!bOsVersionInfoEx) return 1;
+
+	// Call GetNativeSystemInfo if supported or GetSystemInfo otherwise.
+
+	pGNSI = (PGNSI) GetProcAddress(
+		GetModuleHandle(TEXT("kernel32.dll")), 
+		"GetNativeSystemInfo");
+	if(NULL != pGNSI)
+		pGNSI(&si);
+	else GetSystemInfo(&si);
+
+	if ( VER_PLATFORM_WIN32_NT==osvi.dwPlatformId && 
+		osvi.dwMajorVersion > 4 )
+	{
+		StringCchCopy(pszOS, BUFSIZE, TEXT("Microsoft "));
+
+		// Test for the specific product.
+
+		if ( osvi.dwMajorVersion == 6 )
+		{
+			if( osvi.dwMinorVersion == 0 )
+			{
+				if( osvi.wProductType == VER_NT_WORKSTATION )
+					StringCchCat(pszOS, BUFSIZE, TEXT("Windows Vista "));
+				else StringCchCat(pszOS, BUFSIZE, TEXT("Windows Server 2008 " ));
+			}
+
+			if ( osvi.dwMinorVersion == 1 )
+			{
+				if( osvi.wProductType == VER_NT_WORKSTATION )
+					StringCchCat(pszOS, BUFSIZE, TEXT("Windows 7 "));
+				else StringCchCat(pszOS, BUFSIZE, TEXT("Windows Server 2008 R2 " ));
+			}
+
+			pGPI = (PGPI) GetProcAddress(
+				GetModuleHandle(TEXT("kernel32.dll")), 
+				"GetProductInfo");
+
+			pGPI( osvi.dwMajorVersion, osvi.dwMinorVersion, 0, 0, &dwType);
+
+			switch( dwType )
+			{
+			case PRODUCT_ULTIMATE:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Ultimate Edition" ));
+				break;
+			//case PRODUCT_PROFESSIONAL:
+			//	StringCchCat(pszOS, BUFSIZE, TEXT("Professional" ));
+			//	break;
+			case PRODUCT_HOME_PREMIUM:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Home Premium Edition" ));
+				break;
+			case PRODUCT_HOME_BASIC:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Home Basic Edition" ));
+				break;
+			case PRODUCT_ENTERPRISE:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Enterprise Edition" ));
+				break;
+			case PRODUCT_BUSINESS:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Business Edition" ));
+				break;
+			case PRODUCT_STARTER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Starter Edition" ));
+				break;
+			case PRODUCT_CLUSTER_SERVER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Cluster Server Edition" ));
+				break;
+			case PRODUCT_DATACENTER_SERVER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Datacenter Edition" ));
+				break;
+			case PRODUCT_DATACENTER_SERVER_CORE:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Datacenter Edition (core installation)" ));
+				break;
+			case PRODUCT_ENTERPRISE_SERVER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Enterprise Edition" ));
+				break;
+			case PRODUCT_ENTERPRISE_SERVER_CORE:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Enterprise Edition (core installation)" ));
+				break;
+			case PRODUCT_ENTERPRISE_SERVER_IA64:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Enterprise Edition for Itanium-based Systems" ));
+				break;
+			case PRODUCT_SMALLBUSINESS_SERVER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Small Business Server" ));
+				break;
+			case PRODUCT_SMALLBUSINESS_SERVER_PREMIUM:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Small Business Server Premium Edition" ));
+				break;
+			case PRODUCT_STANDARD_SERVER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Standard Edition" ));
+				break;
+			case PRODUCT_STANDARD_SERVER_CORE:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Standard Edition (core installation)" ));
+				break;
+			case PRODUCT_WEB_SERVER:
+				StringCchCat(pszOS, BUFSIZE, TEXT("Web Server Edition" ));
+				break;
+			}
+		}
+
+		if ( osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 2 )
+		{
+			if( GetSystemMetrics(SM_SERVERR2) )
+				StringCchCat(pszOS, BUFSIZE, TEXT( "Windows Server 2003 R2, "));
+			else if ( osvi.wSuiteMask & VER_SUITE_STORAGE_SERVER )
+				StringCchCat(pszOS, BUFSIZE, TEXT( "Windows Storage Server 2003"));
+			//else if ( osvi.wSuiteMask & VER_SUITE_WH_SERVER )
+			//	StringCchCat(pszOS, BUFSIZE, TEXT( "Windows Home Server"));
+			else if( osvi.wProductType == VER_NT_WORKSTATION &&
+				si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_AMD64)
+			{
+				StringCchCat(pszOS, BUFSIZE, TEXT( "Windows XP Professional x64 Edition"));
+			}
+			else StringCchCat(pszOS, BUFSIZE, TEXT("Windows Server 2003, "));
+
+			// Test for the server type.
+			if ( osvi.wProductType != VER_NT_WORKSTATION )
+			{
+				if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_IA64 )
+				{
+					if( osvi.wSuiteMask & VER_SUITE_DATACENTER )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Datacenter Edition for Itanium-based Systems" ));
+					else if( osvi.wSuiteMask & VER_SUITE_ENTERPRISE )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Enterprise Edition for Itanium-based Systems" ));
+				}
+
+				else if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_AMD64 )
+				{
+					if( osvi.wSuiteMask & VER_SUITE_DATACENTER )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Datacenter x64 Edition" ));
+					else if( osvi.wSuiteMask & VER_SUITE_ENTERPRISE )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Enterprise x64 Edition" ));
+					else StringCchCat(pszOS, BUFSIZE, TEXT( "Standard x64 Edition" ));
+				}
+
+				else
+				{
+					if ( osvi.wSuiteMask & VER_SUITE_COMPUTE_SERVER )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Compute Cluster Edition" ));
+					else if( osvi.wSuiteMask & VER_SUITE_DATACENTER )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Datacenter Edition" ));
+					else if( osvi.wSuiteMask & VER_SUITE_ENTERPRISE )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Enterprise Edition" ));
+					else if ( osvi.wSuiteMask & VER_SUITE_BLADE )
+						StringCchCat(pszOS, BUFSIZE, TEXT( "Web Edition" ));
+					else StringCchCat(pszOS, BUFSIZE, TEXT( "Standard Edition" ));
+				}
+			}
+		}
+
+		if ( osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 1 )
+		{
+			StringCchCat(pszOS, BUFSIZE, TEXT("Windows XP "));
+			if( osvi.wSuiteMask & VER_SUITE_PERSONAL )
+				StringCchCat(pszOS, BUFSIZE, TEXT( "Home Edition" ));
+			else StringCchCat(pszOS, BUFSIZE, TEXT( "Professional" ));
+		}
+
+		if ( osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 0 )
+		{
+			StringCchCat(pszOS, BUFSIZE, TEXT("Windows 2000 "));
+
+			if ( osvi.wProductType == VER_NT_WORKSTATION )
+			{
+				StringCchCat(pszOS, BUFSIZE, TEXT( "Professional" ));
+			}
+			else 
+			{
+				if( osvi.wSuiteMask & VER_SUITE_DATACENTER )
+					StringCchCat(pszOS, BUFSIZE, TEXT( "Datacenter Server" ));
+				else if( osvi.wSuiteMask & VER_SUITE_ENTERPRISE )
+					StringCchCat(pszOS, BUFSIZE, TEXT( "Advanced Server" ));
+				else StringCchCat(pszOS, BUFSIZE, TEXT( "Server" ));
+			}
+		}
+
+		// Include service pack (if any) and build number.
+
+		if( _tcslen(osvi.szCSDVersion) > 0 )
+		{
+			StringCchCat(pszOS, BUFSIZE, TEXT(" ") );
+			StringCchCat(pszOS, BUFSIZE, osvi.szCSDVersion);
+		}
+
+		TCHAR buf[80];
+
+		StringCchPrintf( buf, 80, TEXT(" (build %d)"), osvi.dwBuildNumber);
+		StringCchCat(pszOS, BUFSIZE, buf);
+
+		if ( osvi.dwMajorVersion >= 6 )
+		{
+			if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_AMD64 )
+				StringCchCat(pszOS, BUFSIZE, TEXT( ", 64-bit" ));
+			else if (si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_INTEL )
+				StringCchCat(pszOS, BUFSIZE, TEXT(", 32-bit"));
+		}
+
+		return TRUE; 
+	}
+
+	else
+	{  
+		printf( "This sample does not support this version of Windows.\n");
+		return FALSE;
+	}
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if (IsWow64())
+		std::cout << "本机安装的是64位系统" << std::endl;
+	else
+		std::cout << "本机安装的是32位系统" << std::endl;
+
+	TCHAR szOS[BUFSIZE];
+
+	if( GetOSDisplayString( szOS ) )
+		_tprintf( TEXT("\n%s\n"), szOS );
+	
+	std::wstring tmp[] = {
+		_T("{FF66E9F6-83E7-3A3E-AF14-8DE9A809A6A4}"), 
+		_T("{350AA351-21FA-3270-8B7A-835434E766AD}"),
+		_T("{2B547B43-DB50-3139-9EBE-37D419E0F5FA}"),
+
+		_T("{9A25302D-30C0-39D9-BD6F-21E6EC160475}"),
+		_T("{8220EEFE-38CD-377E-8595-13398D740ACE}"),
+		_T("{5827ECE1-AEB0-328E-B813-6FC68622C1F9}"),
+
+		_T("{1F1C2DFC-2D24-3E06-BCB8-725134ADF989}"),
+		_T("{4B6C7001-C7D6-3710-913E-5BC23FCE91E6}"),
+		_T("{977AD349-C2A8-39DD-9273-285C08987C7B}"),
+
+		_T("{9BE518E6-ECC6-35A9-88E4-87755C07200F}"),//x86 32位系统下的重点检查对象
+		_T("{5FCE6D76-F5DC-37AB-B2B8-22AB8CEDB1D4}"),//x64
+		_T("{515643D1-4E9E-342F-A75A-D1F16448DC04}")//ia64
+	};
+	std::vector<std::wstring> strs(tmp, tmp+12);
+	std::wofstream output("D:\\result.txt");
+	int index = 0;
+	for (std::vector<std::wstring>::iterator iter = strs.begin(); iter != strs.end(); ++iter)
+	{
+		output << index++ << std::endl;
+		std::cout << index << std::endl;
+		output << "Check for " << iter->c_str() << std::endl;
+		wprintf(_T("Check for %s\n"), iter->c_str());
+		INSTALLSTATE state = MsiQueryProductState(iter->c_str());
+		switch (state)
+		{
+		case INSTALLSTATE_ABSENT:
+			output << "The product is installed for a different user." << std::endl;
+			std::cout << "The product is installed for a different user." << std::endl;
+			break;
+		case INSTALLSTATE_ADVERTISED:
+			output << "The product is advertised but not installed." << std::endl;
+			std::cout << "The product is advertised but not installed." << std::endl;
+			break;
+		case INSTALLSTATE_DEFAULT:
+			output << "The product is installed for the current user." << std::endl;
+			std::cout << "The product is installed for the current user." << std::endl;
+			break;
+		case INSTALLSTATE_INVALIDARG:
+			output << "An invalid parameter was passed to the function." << std::endl;
+			std::cout << "An invalid parameter was passed to the function." << std::endl;
+			break;
+		case INSTALLSTATE_UNKNOWN:
+			output << "The product is neither advertised or installed." << std::endl;
+			std::cout << "The product is neither advertised or installed." << std::endl;
+			break;
+		}
+	}
+	output.close();
+	std::cout << std::endl;
+	std::cout << "程序执行完成后请传回文件 D:\\result.txt " << std::endl;
+	std::cout << "谢谢" << std::endl;
+	char buf;
+	std::cin >> buf;
+	return 0;
+}
+
